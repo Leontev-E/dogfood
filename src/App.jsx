becd880d9b58ccf74596e35c5ca4c4b8e6f681c5
MIX <evgen.leonev@yandex.ru>
2022-12-03 19:42:00 +0400

    add search

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import React from "react";
-import './style.css';
+import React, {useState} from "react";
+import "./style.css";
+import products from "./assets/data.json";
 
-import Header from './components/Header/header';
-import Footer from './components/Footer/footer';
-import Card from "./components/Card";
+import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
+// import Search from "./components/Search/search";
 
-const smiles = ['*_*', 'O_o', '=)', '-_-', ';(', ':/']
+import Home from "./pages/Home.jsx";
+import Catalog from "./pages/Catalog.jsx";
+
+const smiles = [<span>^_^</span>, "=)", "O_o", ";(", "^_0", "@_@", "–_–"];
 
 const App = () => {
+    const [user, setUser] = useState(localStorage.getItem("user8"));
     return (
         <div className="container">
-            <Header/>
+            <Header user={user} setUser={setUser} products={products}/>
             <main>
-                <h1>Главная страница</h1>
-                <div className="cards">
-                    {/* {smiles} */}
-                    {/* {smiles.map((el, i) => <span key={i}>{el}</span>)} */}
-                    {smiles.map((el, i) => <Card key={'card_' + i} text={el} like={(i + 1) % 2 === 0}/>)}
-                </div>
+                {user ? <Catalog data={products}/> : <Home data={smiles}/>}
             </main>
             <Footer/>
         </div>
     )
 }
-
 export default App;

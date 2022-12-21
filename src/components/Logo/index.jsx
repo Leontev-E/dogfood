8423e7d8d7405ee5009755f564d44efa8f0d818e
MIX <evgen.leonev@yandex.ru>
2022-12-21 23:09:51 +0400

    add catalog

--- src/components/Logo/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import logo from "./img/logo.svg";
-
-export default () => {
-    return <Link className="logo" to="/">
-        <img src={logo} alt="Dog Food"/>
-    </Link>
-}

9d6ac078ffdcb699820cf006d0f05d6cc7762da8
MIX <evgen.leonev@yandex.ru>
2023-01-03 15:15:34 +0400

    add img

--- src/components/Ads/ads.jsx
+++ src/components/Ads/ads.jsx
@@ -5,6 +5,6 @@ import pic from "./img/dogs.png";
 export default () => {
     return <div className="promo">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        <img src={pic} alt="Собачки" />
+        <img src="https://papik.pro/uploads/posts/2021-09/1631769826_22-papik-pro-p-prikolnie-risunki-sobachek-22.png" alt="Собачки" />
     </div>
 }

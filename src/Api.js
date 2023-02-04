b5139cfd3db28824c532f86d720412a1a19e9633
Евгений Леонтьев <evgen.leonev@yandex.ru>
2023-02-04 18:48:39 +0400

    add profile page

--- src/Api.js
+++ src/Api.js
@@ -73,6 +73,16 @@ class Api {
             body: JSON.stringify(body)
         })
     }
+    updUser(body, img = false){
+        return fetch(`${this.path}/v2/${this.group}/users/me${img ? "/avatar" : ""}`, {
+        method: "PATCH",
+        headers: {
+            "authorization": `Bearer ${this.token}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body)
+    })
+    }
 }
 
 export {Api};
